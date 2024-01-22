ab7949e92f794c2c6810a7f637f6c959f860caa3
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-01-22 21:16:55 +0100

    performance optimisations

--- src/main/java/de/craftery/hoppercounter/HopperCounter.java
+++ src/main/java/de/craftery/hoppercounter/HopperCounter.java
@@ -7,18 +7,26 @@ import me.filoghost.holographicdisplays.api.HolographicDisplaysAPI;
 import me.filoghost.holographicdisplays.api.hologram.Hologram;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public final class HopperCounter extends JavaPlugin {
     private static int itemsCollected = 0;
     private static Hologram hologram = null;
     private static HopperCounter instance;
+    private final List<String> registeredCommands = new ArrayList<>();
+    private HopperListener hopperListener;
 
     @Override
     public void onEnable() {
         instance = this;
+
         if (!Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays")) {
             getLogger().severe("*** HolographicDisplays is not installed or not enabled. ***");
             getLogger().severe("*** This plugin will be disabled. ***");
@@ -26,19 +34,9 @@ public final class HopperCounter extends JavaPlugin {
             return;
         }
 
-        PluginCommand command = this.getCommand("sethopper");
-        if (command != null) {
-            SetHopperCommand commandInstance = new SetHopperCommand(this);
-            command.setExecutor(commandInstance);
-            command.setTabCompleter(commandInstance);
-        }
+        this.registerCommand("sethopper", new SetHopperCommand(this));
+        this.registerCommand("setdisplay", new SetDisplayCommand(this));
 
-        command = this.getCommand("setdisplay");
-        if (command != null) {
-            SetDisplayCommand commandInstance = new SetDisplayCommand(this);
-            command.setExecutor(commandInstance);
-            command.setTabCompleter(commandInstance);
-        }
         if (this.getConfig().contains("display.world")) {
             String world = this.getConfig().getString("display.world");
             if (world == null) return;
@@ -51,12 +49,28 @@ public final class HopperCounter extends JavaPlugin {
                     ),
                     this.getConfig().getInt("display.maxAmount")
             );
-
         }
 
         setItemsCollected(this.getConfig().getInt("itemsCollected"));
 
-        new HopperListener(this);
+        hopperListener = new HopperListener(this);
+    }
+
+    private <T extends CommandExecutor & TabCompleter> void registerCommand(String command, T obj) {
+        registeredCommands.add(command);
+        PluginCommand pc = Bukkit.getPluginCommand(command);
+        if (pc == null) return;
+        pc.setExecutor(obj);
+        pc.setTabCompleter(obj);
+    }
+
+    private void unregisterCommands() {
+        for (String command : registeredCommands) {
+            PluginCommand pc = Bukkit.getPluginCommand(command);
+            if (pc == null) continue;
+            pc.setExecutor(null);
+            pc.setTabCompleter(null);
+        }
     }
 
     public static void setDisplay(Location loc, int maxCount) {
@@ -87,10 +101,7 @@ public final class HopperCounter extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        PluginCommand command = this.getCommand("sethopper");
-        if (command != null) {
-            command.setExecutor(null);
-            command.setTabCompleter(null);
-        }
+        unregisterCommands();
+        hopperListener = null;
     }
 }
